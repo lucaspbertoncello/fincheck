c6fce37f68abeac7fd873b8fbc90cbfe0ce8087a
Lucas <lucasbertoncello1@gmail.com>
2025-07-16 01:03:56 -0300

    feat: created auth layout

--- frontend/src/view/layouts/AuthLayout.tsx
+++ frontend/src/view/layouts/AuthLayout.tsx
@@ -1,10 +1,38 @@
 import { Outlet } from "react-router-dom";
 
+import authImage from "../../assets/images/auth-image.png";
+import logo from "../../assets/images/logo.png";
+import grayLogo from "../../assets/images/gray-logo.png";
+
 export function AuthLayout() {
   return (
-    <div>
-      <h1>authlayout</h1>
-      <Outlet />
+    <div className="flex w-full h-full px-8 lg:px-0">
+      {/* form */}
+      <div className="w-full lg:w-1/2 h-full flex items-center justify-center flex-col gap-16">
+        <img src={grayLogo} alt="finCheck logo" />
+
+        <div className="w-full max-w-[440px] flex items-center justify-center">
+          <Outlet />
+        </div>
+      </div>
+
+      {/* auth image */}
+      <div className="w-1/2 h-full p-8 lg:flex items-center justify-center flex-col hidden">
+        <img
+          src={authImage}
+          alt=""
+          className="max-w-[656px] max-h-[960px] w-full h-full object-cover select-none rounded-[32px]"
+        />
+
+        <div className="max-w-[656px] bg-white p-10 absolute rounded-b-[32px] bottom-8">
+          <img src={logo} alt="Fincheck Logo" className="mb-4 select-none" />
+
+          <p className="text-gray-700 font-medium text-xl">
+            Gerencie suas finanças pessoais de uma forma simples com o fincheck,
+            e o melhor, totalmente de graça!
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
