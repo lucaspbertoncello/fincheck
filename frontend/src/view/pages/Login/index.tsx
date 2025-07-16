d59b5e8ebae81be0db0573ea6168a79cad8ea462
Lucas <lucasbertoncello1@gmail.com>
2025-07-16 02:51:46 -0300

    feat: created signup and signin interfaces

--- frontend/src/view/pages/Login/index.tsx
+++ frontend/src/view/pages/Login/index.tsx
@@ -1,3 +1,30 @@
+import { Link } from "react-router-dom";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
 export function Login() {
-  return <div>Login Page</div>;
+  return (
+    <div className="w-full">
+      <header className="flex flex-col items-center gap-4">
+        <h1 className="text-2xl font-bold text-gray-900 tracking-[-1px]">
+          Entre em sua conta
+        </h1>
+
+        <span className="text-gray-700 tracking-[-0.5px]">
+          Novo por aqui?{" "}
+          <Link className="text-teal-900 font-medium" to="/register">
+            Crie uma conta
+          </Link>
+        </span>
+      </header>
+
+      <form className="flex flex-col justify-center gap-4 mt-14">
+        <Input name="email" type="email" placeholder="E-mail" />
+        <Input name="password" type="password" placeholder="Senha" />
+
+        <Button type="submit">Entrar</Button>
+      </form>
+    </div>
+  );
 }
--- frontend/src/view/pages/Register/index.tsx
+++ frontend/src/view/pages/Register/index.tsx
@@ -1,3 +1,31 @@
+import { Link } from "react-router-dom";
+
+import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
+
 export function Register() {
-  return <div>Register Page</div>;
+  return (
+    <div className="w-full">
+      <header className="flex flex-col items-center gap-4">
+        <h1 className="text-2xl font-bold text-gray-900 tracking-[-1px]">
+          Crie sua conta
+        </h1>
+
+        <span className="text-gray-700 tracking-[-0.5px]">
+          Já possuí uma conta?{" "}
+          <Link className="text-teal-900 font-medium" to="/login">
+            Fazer login
+          </Link>
+        </span>
+      </header>
+
+      <form className="flex flex-col justify-center gap-4 mt-14">
+        <Input name="name" type="text" placeholder="Nome" />
+        <Input name="email" type="email" placeholder="E-mail" />
+        <Input name="password" type="password" placeholder="Senha" />
+
+        <Button type="submit">Criar conta</Button>
+      </form>
+    </div>
+  );
 }
