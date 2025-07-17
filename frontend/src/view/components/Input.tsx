dd4ac6e591a185dbd8468cfd49f638fea6b3fb88
Lucas <lucasbertoncello1@gmail.com>
2025-07-17 14:03:58 -0300

    fix: removed name prop

--- frontend/src/view/components/Input.tsx
+++ frontend/src/view/components/Input.tsx
@@ -1,22 +1,20 @@
 import type { ComponentProps } from "react";
 
-interface InputProps extends ComponentProps<"input"> {
-  name: string;
-}
+type InputProps = ComponentProps<"input">;
 
-export function Input({ name, placeholder, ...props }: InputProps) {
+export function Input({ placeholder, ...props }: InputProps) {
   return (
     <div className="relative">
       <input
         className="w-full h-[52px] px-3 outline-0 bg-white rounded-lg border border-gray-500 text-gray-800
         placeholder-shown:pt-0 pt-4 peer focus:border-gray-800 transition-all"
-        id={name}
+        id={placeholder}
         {...props}
         placeholder=" "
       />
 
       <label
-        htmlFor={name}
+        htmlFor={placeholder}
         className="absolute text-xs left-[13px] top-2 pointer-events-none text-gray-900 peer-placeholder-shown:top-3.5
         peer-placeholder-shown:text-base transition-all
         "
