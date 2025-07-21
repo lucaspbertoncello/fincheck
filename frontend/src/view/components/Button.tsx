d4cb83ba2e21a1f3ee05a379c8d482a8e364137d
Lucas <lucasbertoncello1@gmail.com>
2025-07-21 13:49:01 -0300

    feat: apply cn to button component

--- frontend/src/view/components/Button.tsx
+++ frontend/src/view/components/Button.tsx
@@ -1,13 +1,19 @@
 import type { ComponentProps } from "react";
+import { cn } from "../../app/lib/cn";
 
 type ButtonProps = ComponentProps<"button">;
 
-export function Button(props: ButtonProps) {
+export function Button({ className, ...props }: ButtonProps) {
   return (
     <button
-      className="mt-2 w-full bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 disabled:text-gray-400
-      transition-all h-[54px] text-white rounded-2xl cursor-pointer font-medium tracking-[-0.5px]
-      disabled:cursor-not-allowed"
+      className={cn(
+        `
+        mt-2 w-full bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 disabled:text-gray-400
+        transition-all h-[54px] text-white rounded-2xl cursor-pointer font-medium tracking-[-0.5px]
+        disabled:cursor-not-allowed
+      `,
+        className
+      )}
       {...props}
     />
   );
