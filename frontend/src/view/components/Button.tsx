c6706b68c33fac93d88c1635acc2caf0ccb0e57e
Lucas <lucasbertoncello1@gmail.com>
2025-07-25 14:58:22 -0300

    feat: add spinner component to disabled buttons

--- frontend/src/view/components/Button.tsx
+++ frontend/src/view/components/Button.tsx
@@ -1,5 +1,6 @@
 import type { ComponentProps } from "react";
 import { cn } from "../../app/lib/cn";
+import { Spinner } from "./Spinner";
 
 interface ButtonProps extends ComponentProps<"button"> {
   isLoading?: boolean;
@@ -9,6 +10,7 @@ export function Button({
   className,
   isLoading,
   disabled,
+  children,
   ...props
 }: ButtonProps) {
   return (
@@ -17,12 +19,15 @@ export function Button({
         `
         mt-2 w-full bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 disabled:text-gray-400
         transition-all h-[54px] text-white rounded-2xl cursor-pointer font-medium tracking-[-0.5px]
-        disabled:cursor-not-allowed
+        disabled:cursor-not-allowed flex items-center justify-center gap-4
       `,
         className
       )}
       disabled={disabled || isLoading}
       {...props}
-    />
+    >
+      {isLoading && <Spinner />}
+      {children}
+    </button>
   );
 }
