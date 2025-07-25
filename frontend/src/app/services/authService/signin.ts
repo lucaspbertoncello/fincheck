b94a1f6b81a9f9645f91fd09d9975710888731d9
Lucas <lucasbertoncello1@gmail.com>
2025-07-25 14:32:00 -0300

    feat: implemented signin service

---
+++ frontend/src/app/services/authService/signin.ts
@@ -0,0 +1,20 @@
+import { sleep } from "../../lib/sleep";
+import { httpClient } from "../httpClient";
+
+export interface ISigninRequest {
+  email: string;
+  password: string;
+}
+
+interface ISigninResponse {
+  acessToken: string;
+}
+
+export async function signin(params: ISigninRequest) {
+  await sleep(500);
+  const { data } = await httpClient.post<ISigninResponse>(
+    "auth/signin",
+    params
+  );
+  return data;
+}
