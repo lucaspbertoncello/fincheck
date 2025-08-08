8ac0174a5d01b44880754a3ef3fdab73f841a96a
Lucas <lucasbertoncello1@gmail.com>
2025-08-08 16:53:06 -0300

    chore: setup swiper

---
+++ frontend/src/@types/swiper-css.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Type declarations to allow importing Swiper CSS subpath modules in TS.
+ * These modules contain CSS only and export no values.
+ */
+declare module 'swiper/css';
+declare module 'swiper/css/*';
+
+// Optional: if you ever use SCSS entrypoints
+declare module 'swiper/scss';
+declare module 'swiper/scss/*';
