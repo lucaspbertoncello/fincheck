dd925211a52c4ee04b1489c75031eed5fcf2a1a6
Lucas <lucasbertoncello1@gmail.com>
2025-08-12 15:13:07 -0300

    feat(transactions): empty state interface created

--- frontend/src/view/pages/Dashboard/components/Transactions/index.tsx
+++ frontend/src/view/pages/Dashboard/components/Transactions/index.tsx
@@ -11,9 +11,11 @@ import { CategoryIcon } from "../../../../components/icons/categories/CategoryIc
 import { useTransactionsController } from "./useTransactionsController";
 import { cn } from "../../../../../app/lib/cn";
 import { Spinner } from "../../../../components/Spinner";
+import transactionsEmptyState from "../../../../../assets/images/transactions-empty-state.svg";
 
 export function Transactions() {
-  const { areValuesVisible, isLoading } = useTransactionsController();
+  const { areValuesVisible, isLoading, transactions } =
+    useTransactionsController();
 
   return (
     <div className="flex h-full w-full flex-col rounded-2xl bg-gray-100 px-4 py-8 md:p-10">
@@ -57,65 +59,82 @@ export function Transactions() {
           </header>
 
           <div className="mt-4 flex-1 space-y-4 overflow-y-auto">
-            {/* transaction card */}
-            <div className="flex items-center justify-between gap-4 rounded-2xl bg-white p-4">
-              {/* transaction info */}
-              <div className="flex flex-1 items-center gap-3">
-                <CategoryIcon type="expense" />
+            {transactions.length > 0 && (
+              <>
+                {/* transaction card */}
+                <div className="flex items-center justify-between gap-4 rounded-2xl bg-white p-4">
+                  {/* transaction info */}
+                  <div className="flex flex-1 items-center gap-3">
+                    <CategoryIcon type="expense" />
 
-                <div>
-                  <strong className="font-bold tracking-[-0.5px]">
-                    Almoço
-                  </strong>
-                  <span className="block text-sm text-gray-600">
-                    12/08/2025
+                    <div>
+                      <strong className="font-bold tracking-[-0.5px]">
+                        Almoço
+                      </strong>
+                      <span className="block text-sm text-gray-600">
+                        12/08/2025
+                      </span>
+                    </div>
+                  </div>
+                  {/* transaction info */}
+
+                  {/* transaction cost */}
+                  <span
+                    className={cn(
+                      "font-medium tracking-[-0.5px] text-red-800",
+                      !areValuesVisible && "blur-sm",
+                    )}
+                  >
+                    - {formatCurrency(1323.3)}
                   </span>
+                  {/* transaction cost */}
                 </div>
-              </div>
-              {/* transaction info */}
+                {/* transaction card */}
 
-              {/* transaction cost */}
-              <span
-                className={cn(
-                  "font-medium tracking-[-0.5px] text-red-800",
-                  !areValuesVisible && "blur-sm",
-                )}
-              >
-                - {formatCurrency(1323.3)}
-              </span>
-              {/* transaction cost */}
-            </div>
-            {/* transaction card */}
+                {/* transaction card */}
+                <div className="flex items-center justify-between gap-4 rounded-2xl bg-white p-4">
+                  {/* transaction info */}
+                  <div className="flex flex-1 items-center gap-3">
+                    <CategoryIcon type="income" />
 
-            {/* transaction card */}
-            <div className="flex items-center justify-between gap-4 rounded-2xl bg-white p-4">
-              {/* transaction info */}
-              <div className="flex flex-1 items-center gap-3">
-                <CategoryIcon type="income" />
+                    <div>
+                      <strong className="font-bold tracking-[-0.5px]">
+                        Freelancer
+                      </strong>
+                      <span className="block text-sm text-gray-600">
+                        12/08/2025
+                      </span>
+                    </div>
+                  </div>
+                  {/* transaction info */}
 
-                <div>
-                  <strong className="font-bold tracking-[-0.5px]">
-                    Freelancer
-                  </strong>
-                  <span className="block text-sm text-gray-600">
-                    12/08/2025
+                  {/* transaction cost */}
+                  <span
+                    className={cn(
+                      "font-medium tracking-[-0.5px] text-green-800",
+                      !areValuesVisible && "blur-sm",
+                    )}
+                  >
+                    + {formatCurrency(5000.3)}
                   </span>
+                  {/* transaction cost */}
                 </div>
-              </div>
-              {/* transaction info */}
+                {/* transaction card */}
+              </>
+            )}
 
-              {/* transaction cost */}
-              <span
-                className={cn(
-                  "font-medium tracking-[-0.5px] text-green-800",
-                  !areValuesVisible && "blur-sm",
-                )}
-              >
-                + {formatCurrency(5000.3)}
-              </span>
-              {/* transaction cost */}
-            </div>
-            {/* transaction card */}
+            {transactions.length === 0 && (
+              <div className="flex h-full w-full flex-col items-center justify-center">
+                <img
+                  src={transactionsEmptyState}
+                  alt="Nenhuma transação cadastrada"
+                />
+
+                <p className="text-gray-700">
+                  Não encontramos nenhuma transação
+                </p>
+              </div>
+            )}
           </div>
         </>
       )}
