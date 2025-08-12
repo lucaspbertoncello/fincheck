7a6501bc32c498352d7a6fda3aa54653c3c79ab8
Lucas <lucasbertoncello1@gmail.com>
2025-08-12 14:23:45 -0300

    feat(accounts): empty state interface created

--- frontend/src/view/pages/Dashboard/components/Accounts/index.tsx
+++ frontend/src/view/pages/Dashboard/components/Accounts/index.tsx
@@ -8,6 +8,7 @@ import { useAccountsController } from "./useAccountsController";
 import { formatCurrency } from "../../../../../app/utils/formatCurrency";
 import { cn } from "../../../../../app/lib/cn";
 import { Spinner } from "../../../../components/Spinner";
+import { PlusIcon } from "@radix-ui/react-icons";
 
 export function Accounts() {
   const {
@@ -17,6 +18,7 @@ export function Accounts() {
     areValuesVisible,
     toggleValueVisibility,
     isLoading,
+    accounts,
   } = useAccountsController();
 
   return (
@@ -51,59 +53,81 @@ export function Accounts() {
 
           {/* bank accounts area */}
           <div className="flex flex-1 flex-col justify-end">
-            <div>
-              <Swiper
-                spaceBetween={16}
-                slidesPerView={windowWidth >= 500 ? 2.1 : 1.1}
-                onSlideChange={(swiper) => {
-                  setSliderState({
-                    isBeginning: swiper.isBeginning,
-                    isEnd: swiper.isEnd,
-                  });
-                }}
-              >
-                <header
-                  slot="container-start"
-                  className="mb-4 flex items-center justify-between"
+            {accounts.length > 0 && (
+              <div>
+                <Swiper
+                  spaceBetween={16}
+                  slidesPerView={windowWidth >= 500 ? 2.1 : 1.1}
+                  onSlideChange={(swiper) => {
+                    setSliderState({
+                      isBeginning: swiper.isBeginning,
+                      isEnd: swiper.isEnd,
+                    });
+                  }}
                 >
+                  <header
+                    slot="container-start"
+                    className="mb-4 flex items-center justify-between"
+                  >
+                    <strong className="text-lg tracking-[-1px] text-white">
+                      Minhas contas
+                    </strong>
+
+                    <AccountsSliderNavigation
+                      isBeginning={sliderState.isBeginning}
+                      isEnd={sliderState.isEnd}
+                    />
+                  </header>
+
+                  <SwiperSlide>
+                    <AccountCard
+                      color="#7950f2"
+                      bankAccountName="Nubank"
+                      balance={9845.33}
+                      bankAccountIcon="CASH"
+                    />
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <AccountCard
+                      color="#ff7f08"
+                      bankAccountName="Inter"
+                      balance={333.21}
+                      bankAccountIcon="CHECKING"
+                    />
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <AccountCard
+                      color="#ba1e62"
+                      bankAccountName="Bradesco"
+                      balance={100000.0}
+                      bankAccountIcon="INVESTMENT"
+                    />
+                  </SwiperSlide>
+                </Swiper>
+              </div>
+            )}
+
+            {accounts.length === 0 && (
+              <>
+                <header slot="container-start" className="mb-4">
                   <strong className="text-lg tracking-[-1px] text-white">
                     Minhas contas
                   </strong>
-
-                  <AccountsSliderNavigation
-                    isBeginning={sliderState.isBeginning}
-                    isEnd={sliderState.isEnd}
-                  />
                 </header>
 
-                <SwiperSlide>
-                  <AccountCard
-                    color="#7950f2"
-                    bankAccountName="Nubank"
-                    balance={9845.33}
-                    bankAccountIcon="CASH"
-                  />
-                </SwiperSlide>
+                <button className="mt-4 flex h-52 w-full cursor-pointer flex-col items-center justify-center gap-4 rounded-2xl border-2 border-dashed border-teal-600 text-white">
+                  <div className="grid h-11 w-11 place-items-center rounded-full border-2 border-dashed border-white">
+                    <PlusIcon className="h-6 w-6" />
+                  </div>
 
-                <SwiperSlide>
-                  <AccountCard
-                    color="#ff7f08"
-                    bankAccountName="Inter"
-                    balance={333.21}
-                    bankAccountIcon="CHECKING"
-                  />
-                </SwiperSlide>
-
-                <SwiperSlide>
-                  <AccountCard
-                    color="#ba1e62"
-                    bankAccountName="Bradesco"
-                    balance={100000.0}
-                    bankAccountIcon="INVESTMENT"
-                  />
-                </SwiperSlide>
-              </Swiper>
-            </div>
+                  <span className="block w-32 text-center font-medium tracking-[-0.5px]">
+                    Cadastre uma nova conta
+                  </span>
+                </button>
+              </>
+            )}
           </div>
           {/* bank accounts area */}
         </>
