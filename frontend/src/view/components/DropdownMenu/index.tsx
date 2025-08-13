b47332bc1b79738e372f5f3bfdb05481416c2221
Lucas <lucasbertoncello1@gmail.com>
2025-08-13 17:48:03 -0300

    feat: custom radix dropdown component

---
+++ frontend/src/view/components/DropdownMenu/index.tsx
@@ -0,0 +1,13 @@
+import {
+  DropdownContent,
+  DropdownItem,
+  DropdownRoot,
+  DropdownTrigger,
+} from "./Dropdown";
+
+export const DropdownMenu = {
+  Root: DropdownRoot,
+  Trigger: DropdownTrigger,
+  Content: DropdownContent,
+  Item: DropdownItem,
+};
