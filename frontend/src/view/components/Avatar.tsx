c8ec9be247c8ad533929584985427a1e4db6c068
Lucas <lucasbertoncello1@gmail.com>
2025-08-13 18:21:55 -0300

    feat(account): implemented signout button feature

--- frontend/src/view/components/Avatar.tsx
+++ frontend/src/view/components/Avatar.tsx
@@ -1,9 +1,26 @@
+import { ExitIcon } from "@radix-ui/react-icons";
+import { DropdownMenu } from "../components/DropdownMenu";
+import { useAuth } from "../../app/hooks/useAuth";
+
 export function Avatar() {
+  const { signout } = useAuth();
+
   return (
-    <div className="flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-teal-100">
-      <span className="text-sm font-medium tracking-[-0.5px] text-teal-900">
-        LB
-      </span>
-    </div>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <div className="flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-teal-100">
+          <span className="text-sm font-medium tracking-[-0.5px] text-teal-900">
+            LB
+          </span>
+        </div>
+      </DropdownMenu.Trigger>
+
+      <DropdownMenu.Content className="w-32">
+        <DropdownMenu.Item onSelect={signout} className="justify-between">
+          Sair
+          <ExitIcon className="h-4 w-4" />
+        </DropdownMenu.Item>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
   );
 }
