fdc7cd6860a0a0b3f6475d05bd0c1cf3f8ac04c6
Lucas <lucasbertoncello1@gmail.com>
2025-08-14 14:40:41 -0300

    feat(modal): created base modal component

---
+++ frontend/src/view/components/Modal.tsx
@@ -0,0 +1,48 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { cn } from "../../app/lib/cn";
+import { Cross2Icon } from "@radix-ui/react-icons";
+
+interface IModalProps {
+  open: boolean;
+  title: string;
+  children: React.ReactNode;
+  rightAction?: React.ReactNode;
+}
+
+export function Modal({ open, children, title, rightAction }: IModalProps) {
+  return (
+    <Dialog.Root open={open}>
+      <Dialog.Portal>
+        <Dialog.Overlay
+          className={cn(
+            "fixed inset-0 z-50 bg-black/80 backdrop-blur-sm",
+            "modal-overlay",
+          )}
+        />
+
+        <Dialog.Content
+          className={cn(
+            "fixed top-1/2 left-1/2 z-[51] w-full max-w-[400px] -translate-x-1/2 -translate-y-1/2 space-y-10 rounded-2xl bg-white p-6 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10)] outline-none",
+            "modal-content",
+          )}
+        >
+          <header className="flex h-12 items-center justify-between text-gray-800">
+            <button className="h-12 w-12 cursor-pointer">
+              <Cross2Icon className="h-6 w-6" />
+            </button>
+
+            <span className="text-lg font-bold tracking-[-1px]">{title}</span>
+
+            {rightAction && (
+              <div className="grid h-12 w-12 place-items-center">
+                {rightAction}
+              </div>
+            )}
+          </header>
+
+          <div>{children}</div>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+}
