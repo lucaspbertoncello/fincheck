aca059f2a8909335ffd2babd0e9968331f4f514c
Lucas <lucasbertoncello1@gmail.com>
2025-08-14 11:27:08 -0300

    style: created fade-scale in/out to dropdown content

--- frontend/src/view/components/DropdownMenu/Dropdown.tsx
+++ frontend/src/view/components/DropdownMenu/Dropdown.tsx
@@ -25,6 +25,7 @@ export function DropdownContent({ children, className }: IDropdownProps) {
       <RdxDropdownMenu.Content
         className={cn(
           "z-[99] space-y-2 rounded-2xl bg-white p-2 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10)]",
+          "dropdown-content",
           className,
         )}
       >
