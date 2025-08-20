f5660b20930b901a1537cc6cccaa86d5676595b4
Lucas <lucasbertoncello1@gmail.com>
2025-08-20 16:47:08 -0300

    feat: implemented error state for input currency

--- frontend/src/view/components/InputCurrency.tsx
+++ frontend/src/view/components/InputCurrency.tsx
@@ -1,12 +1,26 @@
+import { CrossCircledIcon } from "@radix-ui/react-icons";
 import { NumericFormat } from "react-number-format";
 
-export function InputCurrency() {
+interface IInputCurrencyProps {
+  error?: string;
+}
+
+export function InputCurrency({ error }: IInputCurrencyProps) {
   return (
-    <NumericFormat
-      thousandSeparator="."
-      decimalSeparator=","
-      defaultValue="0.00"
-      className="w-full text-3xl font-bold tracking-[-1px] text-gray-800 outline-none"
-    />
+    <div>
+      <NumericFormat
+        thousandSeparator="."
+        decimalSeparator=","
+        defaultValue="0.00"
+        className="w-full text-3xl font-bold tracking-[-1px] text-gray-800 outline-none"
+      />
+
+      {error && (
+        <div className="mt-2 flex items-center gap-2 text-red-900">
+          <CrossCircledIcon />
+          <span className="text-xs">{error}</span>
+        </div>
+      )}
+    </div>
   );
 }
